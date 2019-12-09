263adc42400370bf41ec76b580a50c0146ee28d3
Sathira <sathirakatugaha@gmail.com>
2019-12-08 19:47:40 -0500

    Initial Arduino files

---
+++ StovetopMonitor.cpp
@@ -0,0 +1,9 @@
+#include "Stovetop.h"
+
+void setup(){
+	//initialize pins here
+}
+
+void loop(){
+	//logic here
+}
---
+++ StovetopMonitor.h
@@ -0,0 +1,5 @@
+// Made by Sathira Katugaha in June of 2018 as a fun project!
+
+// #include dependencies here
+
+//declare global variables = something here
