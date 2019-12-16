5be5f3e05371a8379f841a263d13f57d96099ddf
patrik <patbeqo@gmail.com>
2019-12-15 21:54:39 -0500

    updated the key

--- twilioStoveTop/send_sms.js
+++ twilioStoveTop/send_sms.js
@@ -2,10 +2,10 @@
 // Your Account Sid and Auth Token from twilio.com/console
 // DANGER! This is insecure. See http://twil.io/secure
 const accountSid = 'AC0b57f2bce57db79edf2bed68e5ed7a74';
-const authToken = 'e7e403aeb5a39cf241c274dd6c250493';
+const authToken = '3ef9369f8c656af0487cc148a42050f6';
 const client = require('twilio')(accountSid, authToken);
 
-const client_num = process.argv.find((arg)=>arg.startsWith("--number=")).slice(9);// || '+16046004630';
+const client_num = process.argv.find((arg)=>arg.startsWith("--number=")).slice(9);
 
 if (!client_num) {
   console.log("number not provided");
